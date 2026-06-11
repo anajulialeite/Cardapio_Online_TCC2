e34e7bd0610ea75365a68cd032b3643137aa05d9
Ana Júlia Leite <leite.anajulia25@gmail.com>
2026-06-11 03:37:07 -0300

    fix: mover a imagem da pamonha de doce para a de sal

--- data.js
+++ data.js
@@ -161,10 +161,10 @@ let CATEGORIES = [
       { id: "c2", name: "Cuscuz Queijo Coalho com Ovo", desc: null, price: 19.90, available: true },
       { id: "c3", name: "Cuscuz Muçarela", desc: null, price: 19.90, available: true },
       { id: "c4", name: "Cuscuz Muçarela com Ovo", desc: null, price: 19.90, available: true, image: "images/products/cuscuz_ovo.png" },
-      { id: "c5", name: "Pamonha Sal", desc: null, price: 9.00, available: true },
+      { id: "c5", name: "Pamonha Sal", desc: null, price: 9.00, available: true, image: "images/products/pamonha.png" },
       { id: "c6", name: "Cuscuz Carne Seca com Muçarela", desc: null, price: 22.90, available: true },
       { id: "c7", name: "Cuscuz Calabresa, bacon e muçarela", desc: null, price: 22.90, available: true },
-      { id: "c8", name: "Pamonha Doce", desc: null, price: 9.00, available: true, image: "images/products/pamonha.png" },
+      { id: "c8", name: "Pamonha Doce", desc: null, price: 9.00, available: true },
     ],
     extras: [{ name: "Maionese", price: 0.10 }]
   },
