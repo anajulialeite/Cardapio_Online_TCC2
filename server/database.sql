79d1267c126a325f8ad31a72ed2c6db1820d4e38
Ana Júlia Leite <leite.anajulia25@gmail.com>
2026-07-04 16:17:34 -0300

    Atualização
    Adicionando menagem de proibido plágio nos meus arquivos importantes de back-end.

--- server/database.sql
+++ server/database.sql
@@ -1,3 +1,14 @@
+/**
+ * Projeto: Cardápio Online
+ * Desenvolvido por Ana Júlia de Lima Aguiar Leite
+ * Copyright © 2026 AJ - Criar e Desenvolver. Todos os direitos reservados.
+ *
+ * Este código é destinado exclusivamente para fins acadêmicos e comerciais
+ * da autora. A reprodução, distribuição, modificação, comercialização ou
+ * utilização deste código, total ou parcial, sem autorização expressa da
+ * autora é proibida.
+ */
+
 -- =============================================
 -- CARDÁPIO ONLINE - SCRIPT SQL SERVER
 -- =============================================
